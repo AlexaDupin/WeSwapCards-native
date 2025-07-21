fbf2f5c53a7d5cad795c0a725483228eb08a36f7
Alexa Dupin <alexa.dupin@gmail.com>
2025-07-21 17:34:49 +0200

    chore(constants): added colors

---
+++ constants/Colors.js
@@ -0,0 +1,8 @@
+export const Colors = {
+    primary: "#f07a1a",
+    lightPrimary: "#f5cba9",
+    verylightPrimary: "#fde9d9",
+    darkerPrimary: "#d05e00",
+    secondary: "#16bc90",
+    lightSecondary: "#6cc2ab",
+};
