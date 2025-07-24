bbfd59992ff8d9af3d0f38aa93f9d69252e9abc0
Alexa Dupin <alexa.dupin@gmail.com>
2025-07-24 23:42:47 +0200

    Fix(dashboard): swipeable border

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
 import { ClerkProvider } from '@clerk/clerk-expo';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
+    <GestureHandlerRootView>
     <ClerkProvider tokenCache={tokenCache}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
     </ClerkProvider>
+    </GestureHandlerRootView>
   )
 }
--- components/DashboardItem.tsx
+++ components/DashboardItem.tsx
@@ -1,12 +1,73 @@
+import { useState, useRef } from 'react';
 import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "../assets/styles/dashboard.styles";
 import { Colors } from "../constants/Colors";
+import { Swipeable } from "react-native-gesture-handler";
 
 export const DashboardItem = ({ item, onDelete }) => {
+  const [isSwipeOpen, setIsSwipeOpen] = useState(false);
+  const swipeableRef = useRef(null);
+
+  const renderRightActions = () => (
+      <><TouchableOpacity
+          style={{
+              backgroundColor: Colors.secondary,
+              justifyContent: "center",
+              alignItems: "center",
+              width: 70,
+              marginBottom: 10,
+              alignSelf: 'stretch',
+              borderTopRightRadius: 12,
+              borderBottomRightRadius: 12,
+          }}
+          onPress={() => {
+              onDelete(item.db_id); 
+              swipeableRef.current?.close();
+          }}
+      >
+          <Ionicons name="checkmark-done-outline" size={24} color="white" />
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+          style={{
+              backgroundColor: Colors.primary,
+              justifyContent: "center",
+              alignItems: "center",
+              width: 70,
+              marginBottom: 10,
+              alignSelf: 'stretch',
+            //   borderTopRightRadius: 12,
+            //   borderBottomRightRadius: 12,
+          }}
+          onPress={() => {
+            onDelete(item.db_id);
+            swipeableRef.current?.close();
+          }}
+      >
+              <Ionicons name="remove-circle-outline" size={24} color="white" />
+          </TouchableOpacity></>
+    );
+
   return (
-    <View style={styles.transactionCard} key={item.db_id}>
-      <TouchableOpacity style={styles.transactionContent}>
+    <Swipeable 
+        ref={swipeableRef}
+        renderRightActions={renderRightActions}
+        onSwipeableOpen={() => setIsSwipeOpen(true)}
+        onSwipeableClose={() => setIsSwipeOpen(false)}
+    >
+    <View 
+      style={[
+        styles.transactionCard,
+        {
+          borderTopRightRadius: isSwipeOpen ? 0 : 12,
+          borderBottomRightRadius: isSwipeOpen ? 0 : 12,
+        }
+      ]} 
+      key={item.db_id}
+    >      
+    
+    <TouchableOpacity style={styles.transactionContent}>
         <View style={styles.categoryIconContainer}>
           <Ionicons name={"checkmark-done-outline"} size={22} color={Colors.primary} />
         </View>
@@ -20,5 +81,6 @@ export const DashboardItem = ({ item, onDelete }) => {
         </View>
       </TouchableOpacity>
     </View>
+    </Swipeable>
   );
 };
